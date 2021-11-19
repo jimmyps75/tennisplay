8c2cc930b0186453ded263cbf46eb1028ad8ea44
Jimmy Peñafiel <jimmyps75@gmail.com>
2021-11-19 10:34:06 -0500

    commit inicia jimmy penafiel

---
+++ src/pichincha/com/main.java
@@ -0,0 +1,55 @@
+package pichincha.com;
+import java.util.Scanner;
+
+public class main {
+
+    //declaration and instantiation
+    public static void main( String[] args ) {
+        String jugador = "";
+        boolean continuar;
+        int jugadorPuntajeA = 0, jugadorPuntajeB = 0;
+        int ar[]=new int[2];
+        int a, b, c = 0;
+        Scanner s;
+        do {
+            s = new Scanner( System.in );
+            System.out.println("escribe el literal si el punto es para el jugador A o Jugador B");
+            System.out.println("el punto es para el jugador:");
+            jugador = s.nextLine();
+
+            continuar = acumularPuntaje(ar, jugador);
+            System.out.println("continuar:");
+
+            continuar = s.nextBoolean();
+
+        }while ( continuar);
+
+    }
+
+    public static boolean acumularPuntaje(int[] ar, String jugador){
+
+        ar[2]=ar[2] + 1;
+        switch(jugador) {
+            case "A":
+                ar[0]=ar[0] + 15;
+                //jugadorPuntajeA = jugadorPuntajeA +15;
+
+                break;
+            case "B":
+                ar[1]=ar[1] + 15;
+                //jugadorPuntajeB = jugadorPuntajeA +15;
+
+                break;
+            default:
+                // code block
+        }
+
+        System.out.println("puntuacion jugador A: " + ar[0]);
+        System.out.println("puntuacion jugador B: " + ar[1]);
+
+        return true;
+    }
+
+
+
+}
